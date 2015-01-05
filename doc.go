fd5019d74feb6ab149337725cc6cdd7171848209
Morten Siebuhr <sbhr@sbhr.dk>
2015-01-05 22:44:54 +0100

    Doc: Expand on field naming.

--- doc.go
+++ doc.go
@@ -1,4 +1,8 @@
 // Package gockoboard implements some simple structures matching the Geckoboard
 // custom widgets (https://developer.geckoboard.com) and encodes them to the
-// required go-unfriendly JSON structures.
+// required go-hostile JSON structures.
+//
+// While Geckoboards widget naming is somewhat inconsistent, I've chosen to
+// keep it (and match Geckoboards naming closely), as it saves re-documenting
+// each and every widget.
 package gockoboard
