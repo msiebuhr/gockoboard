76fa3ff788bc275ea7760cffbc1266331e500a86
Morten Siebuhr <sbhr@sbhr.dk>
2015-01-05 22:27:12 +0100

    Implement Trendline + gofmt code

--- widgets.go
+++ widgets.go
@@ -1,30 +1,61 @@
 package gockoboard
 
 import (
-    "encoding/json"
+	"encoding/json"
+	"fmt"
 )
 
 type valueObject struct {
-    Value float64 `json:"value"`
+	Value float64 `json:"value"`
 }
 
 // Implements a Geck-O-Meter.
 // https://developer.geckoboard.com/#geck-o-meter
 type GeckOMeter struct {
-    Item float64
-    Min  float64
-    Max float64
+	Item float64
+	Min  float64
+	Max  float64
 }
 
 // MarshalJSON will marshal the GeckOMeter into JSON.
 func (g GeckOMeter) MarshalJSON() ([]byte, error) {
-    return json.Marshal(struct {
-        Item float64 `json:"item"`
-        Min valueObject `json:"min"`
-        Max valueObject `json:"max"`
-    }{
-        Item: g.Item,
-        Min: valueObject{g.Min},
-        Max: valueObject{g.Max},
-    })
+	return json.Marshal(struct {
+		Item float64     `json:"item"`
+		Min  valueObject `json:"min"`
+		Max  valueObject `json:"max"`
+	}{
+		Item: g.Item,
+		Min:  valueObject{g.Min},
+		Max:  valueObject{g.Max},
+	})
+}
+
+// Trendline implements the geckoboard trendline widget
+// https://developer.geckoboard.com/#trendline-example
+type Trendline struct {
+	Text  string
+	Value float64
+	Trend []float64
+}
+
+// MarshalJSON will marshal the Trendline into JSON.
+//
+// {"item": [{text and value}, [trendline numbers]]}
+func (t Trendline) MarshalJSON() ([]byte, error) {
+	encodedObj, err := json.Marshal(struct {
+		Text  string  `json:"text"`
+		Value float64 `json:"value"`
+	}{Text: t.Text, Value: t.Value})
+
+	if err != nil {
+		return encodedObj, err
+	}
+
+	encodedArray, err := json.Marshal(t.Trend)
+
+	if err != nil {
+		return encodedArray, err
+	}
+
+	return []byte(fmt.Sprintf(`{"item":[%s,%s]}`, encodedObj, encodedArray)), nil
 }
--- widgets_test.go
+++ widgets_test.go
@@ -1,27 +1,46 @@
 package gockoboard
 
 import (
-    "testing"
-    "encoding/json"
-    "fmt"
+	"encoding/json"
+	"testing"
 )
 
 func TestMarshalGeckOMeter(t *testing.T) {
-    g := GeckOMeter{
-        Item: 123.4,
-        Min: 20,
-        Max: 400,
-    }
+	g := GeckOMeter{
+		Item: 123.4,
+		Min:  20,
+		Max:  400,
+	}
 
-    geckoJson, err := json.Marshal(g);
+	geckoJson, err := json.Marshal(g)
 
-    if err != nil{
-        t.Fatalf("Unexpected error when Marshal()'ing:", err)
-    }
+	if err != nil {
+		t.Fatalf("Unexpected error when Marshal()'ing:", err)
+	}
 
-    expectedJson := `{"item":123.4,"min":{"value":20},"max":{"value":400}}`
+	expectedJson := `{"item":123.4,"min":{"value":20},"max":{"value":400}}`
 
-    if string(geckoJson) != expectedJson {
-        t.Fatalf("Expected '%v', got '%v'.", expectedJson, string(geckoJson))
-    }
+	if string(geckoJson) != expectedJson {
+		t.Fatalf("Expected '%v', got '%v'.", expectedJson, string(geckoJson))
+	}
+}
+
+func TestMarshalTrendline(t *testing.T) {
+	tl := Trendline{
+		Text:  "t",
+		Value: 42,
+		Trend: []float64{1, 2, 3},
+	}
+
+	geckoJson, err := json.Marshal(tl)
+
+	if err != nil {
+		t.Fatalf("Unexpected error when Marshal()'ing:", err)
+	}
+
+	expectedJson := `{"item":[{"text":"t","value":42},[1,2,3]]}`
+
+	if string(geckoJson) != expectedJson {
+		t.Fatalf("Expected '%v', got '%v'.", expectedJson, string(geckoJson))
+	}
 }
