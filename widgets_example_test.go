c484c1462fef68440a9ac122413433d1256a744a
Morten Siebuhr <sbhr@sbhr.dk>
2015-01-05 22:41:48 +0100

    Remember to gofmt all code!

--- widgets_example_test.go
+++ widgets_example_test.go
@@ -1,23 +1,23 @@
 package gockoboard
 
 import (
-    "fmt"
-    "os"
-    "encoding/json"
+	"encoding/json"
+	"fmt"
+	"os"
 )
 
 func ExampleTrendline() {
-    tl := Trendline{
-        Text: "Monthly new users",
-        Value: 32,
-        Trend: []float64{2,4,8,16},
-    }
+	tl := Trendline{
+		Text:  "Monthly new users",
+		Value: 32,
+		Trend: []float64{2, 4, 8, 16},
+	}
 
-    b, err := json.Marshal(tl)
-    if err != nil {
-        fmt.Println("error:", err)
-    }
-    os.Stdout.Write(b)
-    // Output:
-    // {"item":[{"text":"Monthly new users","Value":32},[2,4,8,16]]}
+	b, err := json.Marshal(tl)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	os.Stdout.Write(b)
+	// Output:
+	// {"item":[{"text":"Monthly new users","Value":32},[2,4,8,16]]}
 }
