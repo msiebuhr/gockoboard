d6f77d3f2158c4fcd50bb7605053902c3b74d2ce
Morten Siebuhr <sbhr@sbhr.dk>
2015-01-05 22:33:05 +0100

    Docs: Add some real package docs.

---
+++ doc.go
@@ -0,0 +1,4 @@
+// Package gockoboard implements some simple structures matching the Geckoboard
+// custom widgets (https://developer.geckoboard.com) and encodes them to the
+// required go-unfriendly JSON structures.
+package gockoboard
