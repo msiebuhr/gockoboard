f664982c757bd84d6a993d66cff9d046434b1dd9
Morten Siebuhr <sbhr@sbhr.dk>
2015-01-07 21:53:51 +0100

    Move example code alongside other tests.

--- widgets_test.go
+++ widgets_test.go
@@ -2,6 +2,8 @@ package gockoboard
 
 import (
 	"encoding/json"
+	"fmt"
+	"os"
 	"testing"
 )
 
@@ -43,4 +45,20 @@ func TestMarshalNumberAndTrendline(t *testing.T) {
 	if string(geckoJson) != expectedJson {
 		t.Fatalf("Expected '%v', got '%v'.", expectedJson, string(geckoJson))
 	}
+}
+
+func ExampleNumberAndTrendline() {
+	tl := NumberAndTrendline{
+		Text:  "Monthly new users",
+		Value: 32,
+		Trend: []float64{2, 4, 8, 16},
+	}
+
+	b, err := json.Marshal(tl)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	os.Stdout.Write(b)
+	// Output:
+	// {"item":[{"text":"Monthly new users","value":32},[2,4,8,16]]}
 }
